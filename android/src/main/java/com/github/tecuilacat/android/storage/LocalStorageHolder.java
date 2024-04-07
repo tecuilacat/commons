da562a79b15d42576fa5e5cf9d81cd7f80f20168
TecuilaCat <oliver-steck@gmx.de>
2024-04-07 21:33:57 +0200

    updated way to registered files

--- android/src/main/java/com/github/tecuilacat/android/storage/LocalStorageHolder.java
+++ android/src/main/java/com/github/tecuilacat/android/storage/LocalStorageHolder.java
@@ -1,32 +1,27 @@
 package com.github.tecuilacat.android.storage;
 
+import android.content.Context;
+import android.content.ContextWrapper;
+import com.github.tecuilacat.android.filesystem.FileUtlis;
+
 import java.io.File;
-import java.io.IOException;
 import java.time.Instant;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class LocalStorageHolder {
 
-    private static Map<String, LocalStorageFile> registeredStorageFiles;
+    private static final Map<String, LocalStorageFile> registeredStorageFiles = new HashMap<>();
 
     private LocalStorageHolder() { }
 
-    public static void init() {
-        registeredStorageFiles = new HashMap<>();
-    }
-
-    public static LocalStorageFile register(String filename) throws IOException {
-        File file = new File(filename);
+    public static LocalStorageFile register(Context context, String filename) {
+        List<String> filesInProjectFolder = Arrays.stream(context.fileList()).collect(Collectors.toList());
         Date lastModified = Date.from(Instant.now());
-        if (!file.isFile()) {
-            try {
-                file.createNewFile();
-            } catch (IOException ioe) {
-                throw new IOException("Storagefile " + filename + " could not be created", ioe);
-            }
+        if (filesInProjectFolder.stream().noneMatch(fipf -> fipf.equalsIgnoreCase(filename))) {
+            new FileUtlis(context).saveRaw(filename, "");
         } else {
-            lastModified = new Date(file.lastModified());
+            lastModified = new Date(new File(filename).lastModified());
         }
         LocalStorageFile localStorageFile = new LocalStorageFile(filename, lastModified);
         registeredStorageFiles.put(filename, localStorageFile);
