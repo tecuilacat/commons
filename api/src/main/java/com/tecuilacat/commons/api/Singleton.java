9239e865514847c89c01b7338896d29d21c4d95e
TecuilaCat <oliver-steck@gmx.de>
2024-09-30 22:55:14 +0200

    test mit annotationen

---
+++ api/src/main/java/com/tecuilacat/commons/api/Singleton.java
@@ -0,0 +1,8 @@
+package com.tecuilacat.commons.api;
+
+import java.lang.annotation.Retention;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME)
+public @interface Singleton { }
